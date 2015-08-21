454f52187bf3c34f9f649e9218b64b3356a74bf8
Pravin <pravingrg14@gmail.com>
2015-08-20 22:39:38 -0400

    commit all

--- src/test/java/com/synchronisys/automation/practisejava/ArraySample.java
+++ src/test/java/com/synchronisys/automation/practisejava/ArraySample.java
@@ -4,4 +4,5 @@ package com.synchronisys.automation.practisejava;
  * Created by Pravin on 8/20/2015.
  */
 public class ArraySample {
+
 }
--- src/test/java/com/synchronisys/automation/practisejava/PractiseJava.java
+++ src/test/java/com/synchronisys/automation/practisejava/PractiseJava.java
@@ -1,9 +1,31 @@
 package com.synchronisys.automation.practisejava;
 
+import javax.lang.model.element.Name;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Pravin on 8/16/2015.
  */
 public class PractiseJava {
 
+    public static void main(String[] args) {
 
+
+
+        int i = intValue(args[0]);
+        System.out.println(" The i value is " + i );
+        if (i>=5){
+            System.out.println("It is greater than or equal 5");
+        }
+        else{
+            System.out.println("It is less than 5");
+        }
+
+
+    }
+    private static int intValue(String data) {
+        return Integer.parseInt(data);
+    }
 }
+
